dab7bbaf93c49545c63f96f409c59f27f5c41513
Aaron Brown <aaron@9minutesnooze.com>
2012-11-28 17:49:19 -0500

    initial dhcp server

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,3 +3,20 @@ import 'classes/*.pp'
 node default {
   include base
 }
+
+node /^rasp/ {
+  class { 'dhcp' :
+    nameservers => ['192.168.1.4',
+                    '8.8.8.8',
+                    '8.8.4.4'],
+    ntpservers  => ['us.pool.ntp.org'],
+  }
+
+  dhcp::pool { 'borg.lan' :
+    network => '192.168.1.0',
+    mask    => '255.255.255.0',
+    range   => '192.168.1.151 192.168.1.200',
+    gateway => '192.168.1.1',
+  }
+}
+
